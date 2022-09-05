20d5f0500668a9d1a04f4ff9e3795e7a668bc5e9
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-09-05 11:51:29 -0500

    finished exercise inside app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//ES2015 Global Constants
+const PI = 3.14
+
+//Quiz
+//1. Var can be reassinged and redeclared and is function scoped. Let can be reassinged but not redeclared and is block-scoped
+//2. Const cannot be reassigned or redeclared like var, and const is block-scoped.
+//3. Let can be reassigned while const cannot
+//4. Hoisting is when the declaration of a variable using var is run first when the code is executed
